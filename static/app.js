4c5587690ebb0023e9c3bac0678192fda6bb3c07
jake-knox- <jakeknox11@hotmail.co.uk>
2023-06-08 13:54:55 +0100

    styling, minor code changes

--- static/app.js
+++ static/app.js
@@ -4,6 +4,8 @@ const passwordResult = document.getElementById("passwordResult");
 const addButton = document.getElementById("addButton");
 const ulPasswords = document.getElementById("ulPasswords");
 
+addButton.style.visibility = "hidden";
+
 generateButton.addEventListener("click", () => {
     const passwordLength = 10;
     fetch(`/generate-password/${passwordLength}`)
@@ -15,5 +17,15 @@ generateButton.addEventListener("click", () => {
         .catch(error => {
             console.error('Error:', error);
         });
+    
+    if(addButton.style.visibility != "visible")
+        addButton.style.visibility = "visible";
 });
 
+
+addButton.addEventListener("click", () => {
+    console.log("add to list");
+    const newLi = document.createElement("li");
+    newLi.textContent = passwordResult.textContent;
+    ulPasswords.append(newLi);
+})
