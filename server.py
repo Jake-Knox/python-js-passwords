40dea4cadf17b27b121250dd4cd400f58b22191e
jake-knox- <jakeknox11@hotmail.co.uk>
2023-06-08 11:53:48 +0100

    python and html

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+import random
+import string
+
+app = Flask(__name__)
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+@app.route('/generate-password/<int:length>')
+def generate_password_route(length):
+    password = generate_password(length)
+    return jsonify(password=password)
+
+if __name__ == '__main__':
+    app.run()
